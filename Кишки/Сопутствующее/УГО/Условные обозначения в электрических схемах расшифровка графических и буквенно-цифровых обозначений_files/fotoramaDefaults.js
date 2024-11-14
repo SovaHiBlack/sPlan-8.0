12cba00126b84ab5b545a41a86be4593cc011f62
Marketing 1 <Marketing 1@MARKETING1>
2024-11-14 14:35:56 +0300

    Залито всё.

---
+++ Кишки/Сопутствующее/УГО/Условные обозначения в электрических схемах расшифровка графических и буквенно-цифровых обозначений_files/fotoramaDefaults.js
@@ -0,0 +1,10 @@
+fotoramaDefaults = { 
+nav: 'thumbs', 
+navposition: 'top', 
+autoplay: '2000', 
+allowfullscreen: 'true', 
+transition: 'crossfade', 
+loop: true, 
+keyboard: true,
+shuffle: true
+}
